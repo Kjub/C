2a19c57b0bc6cad0456597652ce27028bbd41299
Kjub <kellocube@gmail.com>
2014-05-05 12:32:24 +0200

    - zapocet commit

---
+++ zapocet/zapocet/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  zapocet
+//
+//  Created by Kjub on 05/05/2014.
+//  Copyright (c) 2014 Kjub. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
