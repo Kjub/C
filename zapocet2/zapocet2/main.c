ca705a63fa66921662477eb67b21d1e9d58a090f
Kjub <kellocube@gmail.com>
2014-05-06 18:20:39 +0200

    zapocet2

---
+++ zapocet2/zapocet2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  zapocet2
+//
+//  Created by Kjub on 06/05/2014.
+//  Copyright (c) 2014 Kjub. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
